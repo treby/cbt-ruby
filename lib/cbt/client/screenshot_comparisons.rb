8dc6a148b062f13b78e988f0e7d8097a620dc5a6
Hiroaki Ninomiya <me@treby.info>
2017-09-01 00:49:09 +0900

    feat(screenshot_comparisons): complete whole APIs.

--- lib/cbt/client/screenshot_comparisons.rb
+++ lib/cbt/client/screenshot_comparisons.rb
@@ -1,7 +1,24 @@
 module Cbt
   class Client
     module ScreenshotComparisons
-      def compare_full_screenshot(target_screenshot_test_id, target_version_id, base_version_id, options = {})
+      # Compare Single Screenshot
+      #
+      # @see https://crossbrowsertesting.com/apidocs/v3/screenshot-comparisons.html#!/default/get_screenshots_target_screenshot_test_id_target_version_id_target_result_id_comparison_base_result_id
+      def compare_single_screenshot(target_screenshot_test_id, target_version_id, target_result_id, base_result_id, options = {})
+        connection.get("screenshots/#{target_screenshot_test_id}/#{target_version_id}/#{target_result_id}/comparison/#{base_version_id}", options)
+      end
+
+      # Compare Full Screenshot Test
+      #
+      # @see https://crossbrowsertesting.com/apidocs/v3/screenshot-comparisons.html#!/default/get_screenshots_target_screenshot_test_id_target_version_id_comparison_base_result_id
+      def compare_full_screenshot(target_screenshot_test_id, target_version_id, base_result_id, options = {})
+        connection.get("screenshots/#{target_screenshot_test_id}/#{target_version_id}/comparison/#{base_result_id}", options)
+      end
+
+      # Compare Screenshot Test Version
+      #
+      # @see https://crossbrowsertesting.com/apidocs/v3/screenshot-comparisons.html#!/default/get_screenshots_target_screenshot_test_id_target_version_id_comparison_parallel_base_version_id
+      def compare_screenshot_test_versions(target_screenshot_test_id, target_version_id, base_version_id, options = {})
         connection.get("screenshots/#{target_screenshot_test_id}/#{target_version_id}/comparison/parallel/#{base_version_id}", options)
       end
     end
